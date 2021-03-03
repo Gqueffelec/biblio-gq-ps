d1c456b9f8b308b87a1f34cb610ad13dc93b88e9
59013-71-11 <59013-71-11@exchange.ad.afpanet>
2021-03-03 09:36:57 +0100

    squelette et package de base

---
+++ src/main/java/biblio/servlet/AcceuilServlet.java
@@ -0,0 +1,12 @@
+package biblio.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class AcceuilServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
