cb3fa4b21d455224c641d4e6cbb8b53e7dc686b3
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-03-09 11:15:51 +0100

    update service livre jparepo

--- src/main/java/biblio/service/impl/LivreService.java
+++ src/main/java/biblio/service/impl/LivreService.java
@@ -2,6 +2,7 @@ package biblio.service.impl;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,20 +10,22 @@ import org.springframework.stereotype.Service;
 import biblio.dao.LivreDAO;
 import biblio.dto.LivreDTO;
 import biblio.model.Livre;
+import biblio.repository.IRepositoryLivre;
 import biblio.service.ILibrairieService;
 import biblio.utils.LivreMapper;
 
 @Service
 public class LivreService implements ILibrairieService<LivreDTO> {
 
-	@Autowired
-	private LivreDAO dao;
 	private LivreMapper mapper = new LivreMapper();
+
+	@Autowired
+	private IRepositoryLivre daoTest;
 
 	@Override
 	public List<LivreDTO> getAll() {
 		List<LivreDTO> listeDTO = new LinkedList<>();
-		List<Livre> listeEntity = this.dao.getAll();
+		List<Livre> listeEntity = this.daoTest.findAll();
 		for (Livre entity : listeEntity) {
 			listeDTO.add(mapper.convertToDto(entity));
 		}
@@ -31,22 +34,25 @@ public class LivreService implements ILibrairieService<LivreDTO> {
 
 	@Override
 	public LivreDTO getById(int id) {
-		return mapper.convertToDto(this.dao.getById(id));
+		return mapper.convertToDto(this.daoTest.findById(id).get());
 	}
 
 	@Override
 	public void update(LivreDTO o) {
-		this.dao.update(mapper.convertToEntity(o));
+		Optional<Livre> temp = this.daoTest.findById(o.getId());
+		Livre mappedLivre = mapper.convertToEntity(o);
+		mappedLivre.setId(temp.get().getId());
+		this.daoTest.save(mappedLivre);
 	}
 
 	@Override
 	public void deleteById(int id) {
-		this.dao.remove(id);
+		this.daoTest.deleteById(id);
 	}
 
 	@Override
 	public void add(LivreDTO o) {
-		this.dao.create(mapper.convertToEntity(o));
+		this.daoTest.save(mapper.convertToEntity(o));
 	}
 
 }
