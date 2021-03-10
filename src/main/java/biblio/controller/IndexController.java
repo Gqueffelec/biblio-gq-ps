60d95ea29e318c7565ba1ba75297070a92dd3fdb
Guillaume QUEFFELEC <queffelec.guillaume@gmail.com>
2021-03-10 13:28:48 +0100

    spring mvc inscription

--- src/main/java/biblio/controller/IndexController.java
+++ src/main/java/biblio/controller/IndexController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import biblio.dto.CategorieDTO;
 import biblio.dto.LivreDTO;
+import biblio.service.impl.CategorieService;
 import biblio.service.impl.LivreService;
 
 @Controller
@@ -16,10 +18,15 @@ public class IndexController {
 	@Autowired
 	private LivreService livreService;
 	
+	@Autowired
+	private CategorieService categorieService;
+	
 	@GetMapping({"/","/index"})
 	public String mainPage(ModelMap model) {
 		List<LivreDTO> liste = livreService.getAll();
 		model.addAttribute("livreList",liste);
+		List<CategorieDTO> listeCategorie = this.categorieService.getAll();
+		model.addAttribute("listeCategorie", listeCategorie);
 		return "index";
 	}
 }
